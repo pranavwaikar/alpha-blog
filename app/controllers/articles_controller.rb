4f5f35f24e3398a467dbafe69a2252232ee2e195
pranavwaikar <pranavwaikar@gmail.com>
2020-01-21 11:31:46 +0530

    working with destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :find_article , only:[:edit, :show, :destroy, :update]
+
   def index
     @articles = Article.all
   end
@@ -22,11 +24,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article  = Article.find(params[:id])
+
   end
 
   def update
-    @article  = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Artcle is updated successfully!"
       redirect_to article_path(@article)
@@ -37,12 +38,21 @@ class ArticlesController < ApplicationController
 
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
 
   private
   def article_params
     params.require(:article).permit(:title,:description)
   end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
 end
